7eb16842c7acef28ee15043d1e01582737716fb3
Jeeyong Um <conr2d@proton.me>
2023-05-17 15:58:17 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { es6: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    allowAutomaticSingleRunInference: true,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json', './tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['.eslintrc.cjs'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      },
+    },
+  ],
+};
