e9799eca42371a0030da6898eefc5b08f0793431
Jeeyong Um <conr2d@proton.me>
2023-05-17 16:06:22 +0900

    chore: Extract @noir/util from @noir/api

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Copyright (C) 2023 Haderech Pte. Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+export { decodeAddress, encodeAddress } from './address.js';
+export * from './base64url.js';
+
+export * from '@polkadot/util';
