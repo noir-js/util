7b8ef70163f4770cf22a517e428c516226bcd96a
Jeeyong Um <conr2d@proton.me>
2023-06-20 14:08:27 +0900

    feat: Update UniversalAddress (de)serialization

--- src/address.ts
+++ src/address.ts
@@ -3,16 +3,15 @@
 
 import type { HexString } from '@polkadot/util/types';
 
-import { compactAddLength, compactFromU8aLim, isHex, isU8a, u8aToU8a } from '@polkadot/util';
+import { isHex, isU8a, u8aToU8a } from '@polkadot/util';
 
 import { base64urlToU8a, u8aToBase64url } from './base64url.js';
 
 // eslint-disable-next-line no-unused-vars
 export function encodeAddress (key: HexString | Uint8Array | string, _ss58Format = -1): string {
   const u8a = decodeAddress(key);
-  const [offset] = compactFromU8aLim(u8a);
 
-  return 'u' + u8aToBase64url(u8a.slice(offset));
+  return 'u' + u8aToBase64url(u8a);
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -26,8 +25,8 @@ export function decodeAddress (encoded?: HexString | string | Uint8Array | null,
   }
 
   if (encoded.at(0) !== 'u') {
-    throw new Error('Multibase (base64url) format address is only supported now');
+    throw new Error('Unsupported address format');
   }
 
-  return compactAddLength(base64urlToU8a(encoded.substring(1)));
+  return base64urlToU8a(encoded.substring(1));
 }
