d7546e9de97a60724b5e6cc37177aa4e490e0d93
Jeeyong Um <conr2d@proton.me>
2023-06-20 16:36:05 +0900

    feat: Update UniversalAddress (de)serialization (#2)
    * feat: Update UniversalAddress (de)serialization
    
    * docs: Remove unnecessary lint hints

--- src/address.ts
+++ src/address.ts
@@ -3,19 +3,16 @@
 
 import type { HexString } from '@polkadot/util/types';
 
-import { compactAddLength, compactFromU8aLim, isHex, isU8a, u8aToU8a } from '@polkadot/util';
+import { isHex, isU8a, u8aToU8a } from '@polkadot/util';
 
 import { base64urlToU8a, u8aToBase64url } from './base64url.js';
 
-// eslint-disable-next-line no-unused-vars
 export function encodeAddress (key: HexString | Uint8Array | string, _ss58Format = -1): string {
   const u8a = decodeAddress(key);
-  const [offset] = compactFromU8aLim(u8a);
 
-  return 'u' + u8aToBase64url(u8a.slice(offset));
+  return 'u' + u8aToBase64url(u8a);
 }
 
-// eslint-disable-next-line no-unused-vars
 export function decodeAddress (encoded?: HexString | string | Uint8Array | null, _ignoreChecksum?: boolean, _ss58Format = 1): Uint8Array {
   if (!encoded) {
     throw new Error('Invalid empty address passed');
@@ -26,8 +23,8 @@ export function decodeAddress (encoded?: HexString | string | Uint8Array | null,
   }
 
   if (encoded.at(0) !== 'u') {
-    throw new Error('Multibase (base64url) format address is only supported now');
+    throw new Error('Unsupported address format');
   }
 
-  return compactAddLength(base64urlToU8a(encoded.substring(1)));
+  return base64urlToU8a(encoded.substring(1));
 }
