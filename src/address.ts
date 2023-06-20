2a7b4e7a61a153c51e48f4d026f8f5406107cd29
Jeeyong Um <conr2d@proton.me>
2023-06-20 16:28:52 +0900

    docs: Remove unnecessary lint hints

--- src/address.ts
+++ src/address.ts
@@ -7,14 +7,12 @@ import { isHex, isU8a, u8aToU8a } from '@polkadot/util';
 
 import { base64urlToU8a, u8aToBase64url } from './base64url.js';
 
-// eslint-disable-next-line no-unused-vars
 export function encodeAddress (key: HexString | Uint8Array | string, _ss58Format = -1): string {
   const u8a = decodeAddress(key);
 
   return 'u' + u8aToBase64url(u8a);
 }
 
-// eslint-disable-next-line no-unused-vars
 export function decodeAddress (encoded?: HexString | string | Uint8Array | null, _ignoreChecksum?: boolean, _ss58Format = 1): Uint8Array {
   if (!encoded) {
     throw new Error('Invalid empty address passed');
