8b4f7984c0748d348e449c2add77e5d0507852ef
Jeeyong Um <conr2d@proton.me>
2023-06-20 16:27:04 +0900

    docs: Add documentation for base64url

--- src/base64url.ts
+++ src/base64url.ts
@@ -3,10 +3,18 @@
 
 import { base64urlnopad } from './scure-base.js';
 
+/**
+ * @name u8aToBase64url
+ * @summary Creates a base64url string from a Uint8Array object.
+ */
 export function u8aToBase64url (v: Uint8Array): string {
   return base64urlnopad.encode(v);
 }
 
+/**
+ * @name base64urlToU8a
+ * @summary Creates a Uint8Array object from a base64url string.
+ */
 export function base64urlToU8a (v: string): Uint8Array {
   return base64urlnopad.decode(v);
 }
