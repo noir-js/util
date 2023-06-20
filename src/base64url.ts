a05dc9a2d0af5202b2d1110aaad267ca03ce8487
Jeeyong Um <conr2d@proton.me>
2023-06-20 16:35:50 +0900

    feat: Change base64url decoder to corresponding upstream name (#1)
    * feat: Change base64url decoder to corresponding upstream name
    
    * docs: Add documentation for base64url

--- src/base64url.ts
+++ src/base64url.ts
@@ -1,12 +1,20 @@
 // Copyright (C) 2023 Haderech Pte. Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-import { base64urlUnpadded } from './scure-base.js';
+import { base64urlnopad } from './scure-base.js';
 
+/**
+ * @name u8aToBase64url
+ * @summary Creates a base64url string from a Uint8Array object.
+ */
 export function u8aToBase64url (v: Uint8Array): string {
-  return base64urlUnpadded.encode(v);
+  return base64urlnopad.encode(v);
 }
 
+/**
+ * @name base64urlToU8a
+ * @summary Creates a Uint8Array object from a base64url string.
+ */
 export function base64urlToU8a (v: string): Uint8Array {
-  return base64urlUnpadded.decode(v);
+  return base64urlnopad.decode(v);
 }
